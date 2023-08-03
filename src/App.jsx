ba14e1efbe9cb6fb7df43a11ef676f580844dad3
user1 <code.softwaredevg@gmail.com>
2023-08-03 17:10:25 +0100

    Grid_items

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,57 @@
-import { useState } from 'react'
+/* import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/vite.svg' */
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+/*   const [count, setCount] = useState(0) */
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <section className="bg-[#f6f6f7] py-20 text-center">
+        <div className="mx-auto max-w-7xl px-6 sm:px-8 md:px-12">
+          <h2 className="text-2xl font-semibold md:text-3xl">First Example</h2>
+            <p className="mx-auto mt-3 max-w-xl text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+     {/*  <!-- Grid 1 begins --> */}
+      <div className="mt-12 grid grid-auto-fit-sm gap-8">
+
+         <div className="grid1-item">
+            <span className="bg-yellow-200 ring-yellow-100">
+               <span>1</span>
+            </span>
+              <h3>Ideation</h3>
+                <p>Sequi amet? Aut laboriosam harum repellendus fugit!</p>
+         </div>
+
+         <div className="grid1-item">
+            <span className="bg-blue-200 ring-blue-100">
+               <span>2</span>
+            </span>
+              <h3>Development</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+         </div>
+
+         <div className="grid1-item">
+            <span className="bg-pink-200 ring-pink-100">
+               <span>3</span>
+            </span>
+              <h3>Testing</h3>
+                <p>Rerum reiciendis eius numquam placeat, blanditiis, hic</p>
+         </div>
+
+         <div className="grid1-item">
+            <span className="bg-green-200 ring-green-100">
+               <span>4</span>
+            </span>
+              <h3>Deployment</h3>
+                <p>Labore nam dolorum magni nostrum adipisci.</p>
+         </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* -- Grid 1 ends -- */}
+        </div>
+      </section>
     </>
   )
 }
