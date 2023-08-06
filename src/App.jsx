c138259c864042b33b040dacbe9088978798e120
user1 <code.softwaredevg@gmail.com>
2023-08-06 22:57:46 +0100

    Commit 3x3 Grid

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <div className="container pt-36 items-start">
-        <title className="flex justify-between items-center ml-6 mr-6">Trending Mockups</title>
+        <title className="flex justify-between items-center ml-7 mr-6 pb-8">Available Vehicles</title>
         <div className="items">
           <div className="item first border-2 border-[#00000045]">
             <img src="/icon-sedans.svg"/>
@@ -38,12 +38,13 @@ function App() {
               Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury
               rental and arrive in style.
             </p>
-            <button className=''>Learn more</button>
+            <button>Learn more</button>
           </div>
         </div>
       </div>
 
-      <div className="container">
+      <div className="container pt-0 items-start">
+        <title className="flex justify-between items-center ml-7 mr-6 pb-8">Most Popular</title>
         <div className="items">
           <div className="item first border-2 border-[#00000045]">
             <img src="/icon-sedans.svg"/>
@@ -76,7 +77,46 @@ function App() {
               Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury
               rental and arrive in style.
             </p>
-            <button className=''>Learn more</button>
+            <button>Learn more</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="container pt-0 items-start">
+        <title className="flex justify-between items-center ml-7 mr-6 pb-8">Premium Rentals</title>
+        <div className="items">
+          <div className="item first border-2 border-[#00000045]">
+            <img src="/icon-sedans.svg"/>
+            <h1 className='mt-20'>
+              Sedans
+            </h1>
+            <p>
+              Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city
+              or on your next road trip.
+            </p>
+            <button>Learn more</button>
+          </div>
+          <div className="item second border-2 border-[#00000045]">
+          <img src="/icon-suvs.svg"/>
+          <h1 className='mt-20'>
+              SUVS
+            </h1>
+            <p>
+              Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation
+              and off-road adventures.
+            </p>
+            <button>Learn more</button>
+          </div>
+          <div className="item third border-2 border-[#00000045]">
+          <img src="/icon-luxury.svg"/>
+          <h1 className='mt-20'>
+              Luxury
+            </h1>
+            <p>
+              Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury
+              rental and arrive in style.
+            </p>
+            <button>Learn more</button>
           </div>
         </div>
       </div>
